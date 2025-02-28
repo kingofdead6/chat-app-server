a5061cc15b649345dc49d5a97fcd27ec4c48e547
Young Wolf <162513105+kingofdead6@users.noreply.github.com>
2025-03-01 00:04:44 +0100

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 
 const io = new Server({
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://chat-app-client.vercel.app",
       methods: ["GET", "POST"]
     }
   });
